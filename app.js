36e79edd54a292d034bef75c615c38b5809e37e6
madhurya <madhurya705@gmail.com>
2018-05-06 10:04:36 +0530

    ver_1.002_app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ console.log('starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
-var command = process.argv[2];
-console.log('command', command);
+
+console.log(process.argv)
 
 
